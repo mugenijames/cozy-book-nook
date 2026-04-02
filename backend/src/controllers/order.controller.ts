32bcaeda519eb85d21acabe830247715db362d63
mugenijames <mugenijames99@gmail.com>
2026-04-02 21:03:40 +0300

    updated order.routes.ts

--- backend/src/controllers/order.controller.ts
+++ backend/src/controllers/order.controller.ts
@@ -2,6 +2,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 
+
 export const getOrders = async (req: Request, res: Response) => {
   try {
     const orders = await prisma.order.findMany({
--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -1,6 +1,6 @@
 // backend/src/routes/order.routes.ts
 import { Router } from "express";
-import { createOrder, getOrders, updateOrderStatus, checkOrderStatus } from "../controllers/order.controller";
+import { createOrder, getOrders, updateOrderStatus, checkOrderStatus, getOrderById } from "../controllers/order.controller";
 import { isAdmin } from "../middleware/authMiddleware";
 
 const router = Router();
@@ -9,5 +9,6 @@ router.post("/", createOrder);                          // Public — user submi
 router.get("/", isAdmin, getOrders);                   // Admin — view all orders
 router.put("/:id", isAdmin, updateOrderStatus);        // Admin — approve/reject
 router.get("/check", checkOrderStatus);                // Public — check if approved
+router.get('/:id', getOrderById);
 
 export default router;
